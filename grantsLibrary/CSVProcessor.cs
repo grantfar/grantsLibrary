24228e569774bd7db19884444bf736274201be70
Grant Farnsworth <grantfar@gmail.com>
2015-10-13 18:27:26 -0400

    added write functions to CSV Class

--- grantsLibrary/CSVProcessor.cs
+++ grantsLibrary/CSVProcessor.cs
@@ -49,7 +49,58 @@ namespace grantsLibrary
             }
             return true;
         }
+        /// <summary>
+        /// Deletes the contents of the file and writes a new CSV
+        /// </summary>
+        /// <param name="toWrite"></param>
+        /// <returns>if successful</returns>
+        public bool CsvWriteOveride(String[] toWrite)
+        {
+            return CsvWrite(toWrite,FileMode.OpenOrCreate);
+        }
 
+        public bool CsvWriteAppendEnd(String[] toWrite)
+        {
+            return CsvWrite(toWrite,FileMode.Append);
+        }
+
+        public bool CsvWriteAppendBegining(String[] toWrite)
+        {
+            String[] tmp = mergeLines(csvArray);
+            return CsvWriteOveride(toWrite) & CsvWriteAppendEnd(tmp);
+        }
+
+        public bool CsvWrite(String[] toWrite,FileMode writeType)
+        {
+            StreamWriter writeCsv;
+            try
+            {
+                writeCsv = new StreamWriter(File.Open(csvPath,writeType));
+            }
+            catch (Exception x)
+            {
+                lastError = x.Message;
+                return false;
+            }
+            foreach (String s in toWrite)
+                writeCsv.WriteLine(s);
+            writeCsv.Close();
+            return CsvArrayCreator();
+        }
+
+        private String[] mergeLines(String[][] toMerge)
+        {
+            String[] returnString = new String[toMerge.GetLength(1)];
+            for(int i = 0; i < toMerge.GetLength(1); i++)
+            {
+                String tmp = "";
+                for(int k = 0; k < toMerge.GetLength(0)-1; k++)
+                    tmp += toMerge[i][k] + ",";
+                tmp += toMerge[i][toMerge.GetLength(0)-1];
+                returnString[i] = tmp;
+            }
+            return returnString;
+        }
 
         public String[][] CsvArray
         {
