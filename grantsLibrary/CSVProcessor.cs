7c8a96aa6eef473292e4fcb1f6b8d86b5a8650e6
Grant Farnsworth <grantfar@gmail.com>
2015-10-13 15:48:16 -0400

    created CSV Proccessing Class

---
+++ grantsLibrary/CSVProcessor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace grantsLibrary
+{
+    public class CSVProcessor
+    {
+        private System.IO.StreamReader csvReader;
+        private String csvPath;
+        protected String[][] csvArray;
+        protected String lastError;
+
+        
+        public CSVProcessor(String csvPathString)
+        {
+            csvPath = csvPathString;
+        }
+        public CSVProcessor()
+        {
+        }
+
+        public bool CsvArrayCreator()
+        {
+            try
+            {
+                csvReader = new System.IO.StreamReader(csvPath);
+            }
+            catch(Exception x)
+            {
+                lastError = x.Message;
+                return false;
+            }
+            String[] tempWineArray;
+            List<String> wineList = new List<string>();
+            while (!csvReader.EndOfStream)
+            {
+                wineList.Add(csvReader.ReadLine());
+            }
+            csvReader.Close();
+            csvArray = new String[wineList.Count][];
+            for (int i = 0; i < wineList.Count; i++)
+            {
+                tempWineArray = wineList[i].Split(',');
+                csvArray[i] = tempWineArray;
+            }
+            return true;
+        }
+
+
+        public String[][] CsvArray
+        {
+            get { return csvArray; }
+        }
+        public String LastError
+        {
+            get { return lastError; }
+        }
+        public String CsvPath
+        {
+            get { return csvPath; }
+            set { csvPath = value; }
+        }
+    }
+}
